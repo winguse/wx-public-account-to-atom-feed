822410aa5b32b6f1331b1760707121a4e0151767
Yingyu Cheng <github@winguse.com>
2016-07-17 23:45:39 +0800

    added debug fun

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch';
-
-async function fetchWebTest(url:string) {
-	const response = await fetch(url);
-	const text = await response.text();
-	console.log(text);
-}
-
-fetchWebTest('https://winguse.com');
